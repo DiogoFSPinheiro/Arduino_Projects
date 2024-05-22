3db6c166d6489be26148d12e1f8f5bd305884bef
DiogoFSPinheiro <143399294+DiogoFSPinheiro@users.noreply.github.com>
2024-05-22 17:20:25 +0100

    Update RGB_Flow.ino

--- RGB_Flow/RGB_Flow.ino
+++ RGB_Flow/RGB_Flow.ino
@@ -1,16 +1,14 @@
 #define  led_red 11
 #define  led_gre 10
 #define  led_blu 9
-#define wait_time 2000
+#define  wait_time 2000
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
   pinMode(led_red, OUTPUT);
   pinMode(led_gre, OUTPUT);
   pinMode(led_blu, OUTPUT);
-
-  // put your setup code here, to run once:
-
 }
 
 void flow(int led)
@@ -18,22 +16,18 @@ void flow(int led)
   int color = 0;
   while(color++ < 255)
   {
-   analogWrite(led, color);
-   delay(5);
-
+    analogWrite(led, color);
+    delay(5);
   }
   while(color-- > 0)
-   {analogWrite(led, color);
-   delay(5);
-   }
-
+  {
+    analogWrite(led, color);
+    delay(5);
+  }
 }
 
-void loop() {
-    int l = random(9,12);
-    flow(l);
-    Serial.print(l);
+void loop() 
+{
+    flow(random(9,12));
     delay(500);
-    
-
 }
